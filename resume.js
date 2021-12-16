f2f5f0b62407044afca599796975a8fe7aea9af9
Purna Shakya <38076182+landxcape@users.noreply.github.com>
2021-12-16 18:18:22 +0545

    add json formated

--- resume.js
+++ resume.js
@@ -1,41 +1,43 @@
 function resumeJson() {
     var jsonData = {
-        'name': 'Purna Kalyan Shakya',
-        'location': {
-            'permanent': 'Narayangadh, Chitwan, Nepal',
-            'temporaty': 'Kathmandu, Bagmati, Nepal',
+        name: 'Purna Kalyan Shakya',
+        location: {
+            permanent: 'Narayangadh, Chitwan, Nepal',
+            temporaty: 'Kathmandu, Bagmati, Nepal',
         },
-        'contact': {
-            'gmail': 'landxcape09@gmail.com',
-            'linkedin': 'linkedin.com/in/purnashakya',
-            'github': 'github.com/landxcape',
+        contact: {
+            gmail: 'landxcape09@gmail.com',
+            linkedin: 'linkedin.com/in/purnashakya',
+            github: 'github.com/landxcape',
         },
-        'summary': [
+        summary: [
             'Electronics and Communication Engineer',
             'Programmer',
         ],
-        'experience': [{
-            'title': 'Programmer',
-            'organization': 'Engineering Solutions for Nepal Pvt. Ltd.',
-            'duration': 'Jun 2019 - Jan 2021 (1 year 8 months)',
+        experience: [{
+            title: 'Programmer',
+            organization: 'Engineering Solutions for Nepal Pvt. Ltd.',
+            duration: 'Jun 2019 - Jan 2021 (1 year 8 months)',
         }],
-        'education': [{
-            'university': 'Pokhara University',
-            'college': 'Nepal Engineering College',
-            'degree': 'Bachelor of Electronics and Communication Engineering',
-            'year': '2014 - 2019',
+        education: [{
+            university: 'Pokhara University',
+            college: 'Nepal Engineering College',
+            degree: 'Bachelor of Electronics and Communication Engineering',
+            year: '2014 - 2019',
         }],
-        'licensesAndCertifications': [{
-            'title': 'Electronics and Communication Engineer',
-            'issuingOrganization': 'Nepal Engineering Council',
+        licensesAndCertifications: [{
+            title: 'Electronics and Communication Engineer',
+            issuingOrganization: 'Nepal Engineering Council',
         }],
-        'skills': {
-            'programming': {
-                'languages': ['Python', 'C', 'C++', 'Arduino'],
-                'tools': ['git', 'Django', 'Flutter', 'RaspberryPi'],
+        skills: {
+            programming: {
+                languages: ['Python', 'C', 'C++', 'Arduino',],
+                tools: ['git', 'Django', 'Flutter', 'RaspberryPi',],
             }
         }
     };
 
     document.getElementById('resume').innerHTML = JSON.stringify(jsonData, undefined, 2);
 }
+
+document.addEventListener('DOMContentLoaded', () => resumeJson());
