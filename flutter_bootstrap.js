71acaa7ba1b28be20b05a0ce6e8fa68e8f7bad28
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2026-02-14 01:23:18 +0545

    change build to canvaskit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2498212244" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "282291052" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
