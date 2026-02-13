f5a94166321e16ba528515ae4f7caaf05be93a26
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2026-02-14 01:09:59 +0545

    revert: Remove WASM build support
    - Revert flutter_bootstrap.js to only include dart2js build
    - Remove WASM-related files (main.dart.wasm, main.dart.mjs, and their maps)
    - Update service worker version to 2498212244

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b17f","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b17f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "439168393" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2498212244" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
